440b33b864a8f6fb37f6b04b6b800f81b9af432c
phibzy <phibzy@gmail.com>
2020-12-11 14:43:53 +1100

    Rearranging data

--- CovidMapStats/covid.py
+++ CovidMapStats/covid.py
@@ -17,6 +17,7 @@ logging.basicConfig(level=logging.DEBUG, format="%(msg)s")
 
 # Uncomment this to disable debugging output
 # logging.disable(logging.DEBUG)
+# CLI = Covid-like illness
 
 #TODO: class format for easy testing
 
@@ -35,26 +36,87 @@ def convertDateToAU(date: str) -> str:
 # and I happen to be Australian
 aussieDateFormat = True
 
+# Will do checks later on when data returns
 # TODO: country/region checkers
-country = "Australia"
+country = "Australia" # Mandatory
 region = "New South Wales"
 
 # TODO: Might as well check valid fields too
-indicator = "mask"
-typ = "daily"
+indicator = "mask" # Mandatory
+typ = "daily"      # Mandatory
 
 # Dates must be in format YYYYMMDD - TODO: Add tests for invalid ranges, test one day range
-# TODO: Invalid date checkers
 dateStart = "01102020"
 dateEnd   = "31102020"
 
 # Say what fields you want out of this data
 # TODO: Check field possibilities for each set of data
 fields = {
-    'percent_mc_unw',
-    'percent_mc',
+    # Present in all query results
+    'country',
+    'iso_code',
+    'gid_0',
     'sample_size',
-    'survey_date'
+
+    # Present if region given
+    "region",
+    "gid_1",
+
+    # cli - covid indicator fields
+
+    # ili - flu indicator fields
+
+    'percent_mc_unw',
+    'percent_mc'
+}
+fields = {
+    "country": "country name of the data.",
+    "gid_0": "the code for join country level data to the GADM country level data",
+    "sample_size": "sample size for calculating the targeted value.",
+    "survey_date": "date when survey was taken",
+    "iso_code": "2 or 3 letter ISO code of country",
+
+
+    # if region specified
+    "region": "sub-country region name of the data. Generally, it is at state or province level or equivalent.",
+    "gid_1": "the code for join region level data to the GADM region level data",
+
+    # covid fields - daily
+   "percent_cli": "weighted percentage of survey respondents that have reported CLI.",
+   "cli_se": "standard error of percent_cli.",
+   "percent_cli_unw": "unweighted percentage of survey respondents that have reported CLI.",
+   "cli_se_unw": " standard error of percent_cli_unw.",
+
+    # covid fields - smoothed
+   "smoothed_cli": "seven-day rolling average of percent_cli values.",
+   "smoothed_cli_se": " standard error of smoothed_cli.",
+
+
+
+   "percent_ili": "weighted percentage of survey respondents that have reported ILI.",
+   "percent_mc": "weighted percentage of survey respondents that have reported use mask cover.",
+   "percent_dc": "weighted percentage of survey respondents that have reported had direct contact  (longer than one minute) with people not staying with them in last 24 hours.",
+   "percent_hf": "weighted percentage of survey respondents that are worried about themselves and their household’s finances in the next month.",
+   "percent_ili_unw": "unweighted percentage of survey respondents that have reported ILI.",
+   "percent_mc_unw": "unweighted percentage of survey respondents that have reported use mask cover.",
+   "percent_dc_unw": "unweighted percentage of survey respondents that have reported use have direct contact with people not staying with them.",
+   "percent_hf_unw": "unweighted percentage of survey respondents that are worried about themselves and their household’s finances in the next month.",
+   "smoothed_ili": "seven-day rolling average of percent_ili values.",
+   "smoothed_mc": "seven-day rolling average of percent_mc values.",
+   "smoothed_dc": "seven-day rolling average of percent_dc values.",
+   "smoothed_hf": "seven-day rolling average of percent_hf values.",
+   "smoothed_ili_se": "standard error of smoothed_ili.",
+   "smoothed_mc_se": "standard error of smoothed_mc.",
+   "smoothed_dc_se": "standard error of smoothed_dc.",
+   "smoothed_hf_se": "standard error of smoothed_hf.",
+   "ili_se": "standard error of percent_ili.",
+   "mc_se": "standard error of percent_mc.",
+   "dc_se": "standard error of percent_dc.",
+   "hf_se": "standard error of percent_hf.",
+   "ili_se_unw": " standard error of percent_ili_unw.",
+   "mc_se_unw": " standard error of percent_mc_unw.",
+   "dc_se_unw": " standard error of percent_dc_unw.",
+   "hf_se_unw": " standard error of percent_hf_unw.",
 }
 
 ###########################
@@ -92,7 +154,7 @@ jsonData = response.json()
 
 output = list()
 
-
+# TODO: Error handling if there's no data at all in daterange
 for i in jsonData['data']:
     # For each entry in data dict, grab the key/value pairs
     # for the specified fields that we want!
